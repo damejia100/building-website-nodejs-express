8dc9ed9ad92c724e84d4a4dc5540bb4a55160592
devAM100 <devismejia@gmail.com>
2020-07-29 16:33:54 -0400

    serving home and speakers page

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const path = require('path')
 
 const app = express();
 
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, './static')))
+
 app.get('/', (req, res, next) => {
-  res.send('Hello express here')
+  res.sendFile(path.join(__dirname, './static/index.html'))
+})
+
+app.get('/speakers', (req, res, next) => {
+  res.sendFile(path.join(__dirname, './static/speakers.html'))
 })
 
 app.listen(port, () => {
