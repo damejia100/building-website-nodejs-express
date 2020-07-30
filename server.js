44e0a2e6965832995ce4d1bbd4568ce4bda7b034
devAM100 <devismejia@gmail.com>
2020-07-30 17:57:37 -0400

    ejs connected;

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.set('views', path.join(__dirname), './views')
 app.use(express.static(path.join(__dirname, './static')))
 
 app.get('/', (req, res, next) => {
-  res.render('pages/index', {pageTitle: 'Welcome'})
+  res.render('pages/index.ejs', {pageTitle: 'Welcome'})
 })
 
 app.get('/speakers', (req, res, next) => {
